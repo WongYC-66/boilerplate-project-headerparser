ed4b1700194d77f9ec5d9f0873a832a8915d80cd
YC Wong <yengchien@hotmail.com>
2023-05-26 22:56:54 +0800

    Test : pass test

--- index.js
+++ index.js
@@ -11,20 +11,44 @@ var app = express();
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
+// body-parser
+let bodyParser = require('body-parser');
+// Mount body parser before other uses it
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  const headers = req.headers
+  console.log(headers)
+  const userSoftware = headers['user-agent']
+  const userLanguage = headers['accept-language']
+  const userIpAddress = headers['x-forwarded-for']
+
+  // print to browser
+  res.json({
+    ipaddress: userIpAddress,
+    language: userLanguage,
+    software: userSoftware
+  });
+  // res.json({ greeting: 'whoami' });
+
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
